700a05a70b7d82956a77d0ae7ad620def3ae5205
Abir Dutta <abirdutta390@gmail.com>
2024-07-24 11:16:42 +0530

    scrollbar hide at home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import Cover from "@/components/Cover";
 import CoverLoggedin from "@/components/CoverLoggedin";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { ScrollShadow } from "@nextui-org/scroll-shadow";
 
 export default function Home() {
   return (
-    <h1 className="flex items-center justify-center h-[75vh] px-5">
+    <ScrollShadow hideScrollBar className="w-full h-[85vh]">
+    <div className="flex items-center justify-center h-[75vh] px-5">
       <SignedOut>
         <Cover />
       </SignedOut>
       <SignedIn>
         <CoverLoggedin />
       </SignedIn>
-    </h1>
+    </div>
+    </ScrollShadow>
   );
 }
