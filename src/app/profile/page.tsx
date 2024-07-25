78ffca5c89a91de44e11e2e5a4c626687898a0fb
Abir Dutta <abirdutta390@gmail.com>
2024-07-25 22:23:29 +0530

    responsive navbar

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,9 @@
+// import { useUser } from "@clerk/clerk-react";
+
+export default function Profile(){
+    // const { user } = useUser();
+    // const username = user?.fullName;
+    return (
+        <div className="px-4">Hello there</div>
+    )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,28 @@
+'use client'
 import React from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  Link,
-  Button,
-} from "@nextui-org/react";
 import nextLink from "next/link";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton,SignOutButton } from "@clerk/nextjs";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
 
-export default function Header() {
+
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   return (
-    <Navbar className="border-b border-gray-800 mb-5 ">
-      <NavbarBrand>
-        <SignedOut>
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
           <Link as={nextLink} href="/" color="foreground">
             <p className=" font-bold text-inherit italic">Bloggify</p>
           </Link>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </NavbarBrand>
-      <NavbarContent className="md:flex gap-4" justify="start">
-        <SignedIn>
-          <NavbarItem>
-            <Link
-              as={nextLink}
-              className="hover:bg-zinc-800 rounded-md px-4 py-2  transition-all duration-250 hover:text-gray-50"
-              color="foreground"
-              href="/"
-            >
-              Home
-            </Link>
-          </NavbarItem>
-        </SignedIn>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
           <Link
             as={nextLink}
@@ -45,9 +32,16 @@ export default function Header() {
           >
             Posts
           </Link>
-        </NavbarItem>
+        </NavbarItem>                
       </NavbarContent>
       <NavbarContent justify="end">
+      <SignedIn>
+        <NavbarItem >
+          <div className="bg-zinc-800 hover:bg-transparent transition-all duration-500 border border-gray-200 rounded-lg px-2 py-1 ">
+              <SignOutButton>Sign Out</SignOutButton>
+            </div>
+        </NavbarItem>
+        </SignedIn>
         <SignedOut>
           <NavbarItem>
             <div className="bg-zinc-800 hover:bg-transparent transition-all duration-500 border border-gray-200 rounded-lg px-2 py-1 ">
@@ -71,6 +65,46 @@ export default function Header() {
           </SignedIn>
         </div>
       </NavbarContent>
+      <NavbarMenu>
+        
+          <NavbarMenuItem>
+          <Link
+            as={nextLink}
+            className="hover:bg-zinc-800 rounded-md px-4 py-2  transition-all duration-250 hover:text-gray-50"
+            href="/posts"
+            color="foreground"
+          >
+            Posts
+          </Link>
+          </NavbarMenuItem>
+          <SignedIn>
+          <NavbarMenuItem>
+          <Link
+            as={nextLink}
+            className="hover:bg-zinc-800 rounded-md px-4 py-2  transition-all duration-250 hover:text-gray-50"
+            href={`/profile`}
+            color="foreground"
+          >
+            Profile
+          </Link>
+          </NavbarMenuItem>
+          </SignedIn>
+          <SignedIn>
+          <NavbarMenuItem>
+          <Link
+            as={nextLink}
+            className="hover:bg-zinc-800 rounded-md px-4 py-2  transition-all duration-250 hover:text-gray-50"
+            href={`/new-blog`}
+            color="foreground"
+          >
+            Create Post
+          </Link>
+          </NavbarMenuItem>
+          </SignedIn>
+      </NavbarMenu>
     </Navbar>
   );
 }
+
+
+
